f99e960f5ed6c1e239ab570ca4e1b8964542f3f0
Ulises Rodríguez <ulisesmercer9@gmail.com>
2020-10-20 16:20:23 -0500

    feat: change variable names and add new message link

--- correos.php
+++ correos.php
@@ -1,17 +1,20 @@
 <?php   
-    $destinatario = 'leadshhv@bolsarosa.com';
+    //$destinatario = 'leadshhv@bolsarosa.com';
 
-    $name = $_POST['name'];
+    $destinatario = 'ulisesmercer9@gmail.com';
+
+    $nombre = $_POST['nombre'];
     $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    $company = $_POST['company'];
-    $position = $_POST['position'];
-    $state = $_POST['state'];
+    $telefono = $_POST['telefono'];
+    $compañia = $_POST['compañia'];
+    $posicion = $_POST['posicion'];
+    $estado = $_POST['estado'];
 
     $asunto = 'Lead landing HeadHunter';
+    $mensaje = "La información puede ser consultada en: https://docs.google.com/spreadsheets/d/1uKD3-fk5qtQZEb1VjJn8hIV6PccOBGzvh4ig-ohlfHQ/edit#gid=0";
 
     $header = "Enviado desde la página de Landing page HeadHunter";
-    $contenido = "Email: " . $email ."\nTelefono: " . $phone . "\nCompañia: " . $company . "\nPosición: " . $position . "\nEstado: " . $state . "\nAtentamente: " . $name;
+    $contenido = "Email: " . $email ."\nTelefono: " . $telefono . "\nCompañia: " . $compañia . "\nPosición: " . $posicion . "\nEstado: " . $estado . "\nAtentamente: " . $nombre . "\n" . $mensaje;
 
     mail($destinatario, $asunto, $contenido, $header);
     echo "<script>alert('Solicitud de contacto enviada exitosamente, nos pondremos en contacto contigo muy pronto ya puedes cerrar esta página.')</script>";
